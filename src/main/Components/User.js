a602688acc504e69896d25e07454bf5d4b5868e8
hugo <hugo@medeo-health.com>
2017-06-14 17:17:50 +0200

    user page

--- src/main/Components/User.js
+++ src/main/Components/User.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
 
 class User extends React.Component {
     render() {
         return(
-            <h1>Page User</h1>
+            <div>
+                <h4>Informations sur l'utilisateur</h4>
+                <div className="col-xs-6">
+                    <label htmlFor="firstName" className="col-xs-12">Prénom : {this.props.user.firstName}</label>
+                    <label htmlFor="lastName" className="col-xs-12">Nom : {this.props.user.lastName}</label>
+                    <label htmlFor="email" className="col-xs-12">Mail : {this.props.user.email}</label>
+                    <label htmlFor="company" className="col-xs-12">Entreprise : {this.props.user.company}</label>
+                </div>
+            </div>
         )
     }
 }
 
-export default User;
+const mapStateToProps = (state) => ({
+    user: state.user
+});
+
+export default connect(mapStateToProps, null)(User);
